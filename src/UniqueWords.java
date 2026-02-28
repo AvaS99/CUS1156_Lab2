e5ba954add120bd71e763aed30de1517a8c2efc1
Ava Soric <ava100606@gmail.com>
2026-02-27 21:24:17 -0500

    Initial commit

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,37 +1,56 @@
+/* Project: Lab3
+ * Class: UniqueWords.java
+ * Author: Ava Soric
+ * Date: February 26th, 2026
+ * This program counts the number of distinct elements in an
+ * ArrayList<String> of different words.
+ */
+
 import java.util.ArrayList;
 
-public class UniqueWords
-{
-   /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
-	  return count;
-   }
+public class UniqueWords {
+	/**
+	 * counts the number of unique strings in a list
+	 * 
+	 * @param list ArrayList of strings to be examined
+	 * @return number of unique strings in the list
+	 */
+	
+	// ----------------------------------------------------------------
+	// Compares all Strings to each other and only counts if unique
+	public static int countUnique(ArrayList<String> list) {
+		int count = 0;
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
-      words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
-      System.out.println(words + " has " + unique + " unique words");
-   }
+		for (int i = 0; i < list.size(); i++) {
+			boolean uniqueWord = true;
+
+			for (int j = 0; j < i; j++) {
+				if (list.get(i).equals(list.get(j))) {
+					uniqueWord = false;
+					break;
+				}
+			}
+			if (uniqueWord) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	// ----------------------------------------------------------------
+	// Creates a String ArrayList of words and uses countUnique
+	// to determine the number of distinct, unique words
+	public static void main(String[] args) {
+		ArrayList<String> words = new ArrayList<>();
+		words.add("apple");
+		words.add("orange");
+		words.add("blackboard");
+		words.add("apple");
+		words.add("orange");
+		words.add("sun");
+		words.add("moon");
+
+		int unique = countUnique(words);
+		System.out.println(words + " has " + unique + " unique words");
+	}
 }
